347d63cbc3e5f5e61c988fb34463c332a30d7d8a
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-09 16:00:18 -0600

    Modify the destroy controlles to delete the foods associated with the recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,13 @@ class RecipesController < ApplicationController
   def destroy
     @recipe = Recipe.find(params[:id])
     authorize! :destroy, @recipe
+    @foods_list = RecipeFood.where(recipe_id: @recipe)
+
     RecipeFood.where(recipe_id: @recipe).delete_all
+
+    @foods_list.each do |food|
+      Food.where(id: food.food_id).delete_all
+    end
 
     return unless @recipe.destroy
 
