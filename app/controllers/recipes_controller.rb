adec0c2922529f630df198efbbca40b0bbaf3690
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-09 15:36:31 -0600

    Create index and destroy actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,18 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @user = current_user
+    @recipes = @user.recipes
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    authorize! :destroy, @recipe
+    RecipeFood.where(recipe_id: @recipe).delete_all
+
+    return unless @recipe.destroy
+
+    redirect_to recipes_path
+  end
 end
