51e9a420f283574eb8a598fedb11035ab5767c60
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-11 02:10:58 -0600

    Add the new, create, edit, update and destroy actions

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -4,11 +4,58 @@ class RecipeFoodsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @user = current_user
+    @food_list = @user.foods
+    @recipe = Recipe.find(params[:id])
 
-  def create; end
+    @foods = filter_foods(@recipe, @food_list)
 
-  def edit; end
+    @new_recipe_food = @recipe.recipe_foods.new
+  end
 
-  def destroy; end
+  def create
+    recipe_food = RecipeFood.new(recipe_food_params)
+    if recipe_food.save
+      redirect_to new_recipe_food_path(recipe_food.recipe_id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @recipe_food = RecipeFood.find(params[:id])
+    @food = Food.find(@recipe_food.food_id)
+  end
+
+  def update
+    @recipe_food = RecipeFood.find(params[:id])
+    return unless @recipe_food.update(update_params)
+
+    redirect_to recipe_path(@recipe_food.recipe_id)
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.where(food_id: params[:id])
+    recipe = Recipe.where(id: @recipe_food[0].recipe_id)
+    return unless @recipe_food.delete_all
+
+    redirect_to recipe_path(recipe[0].id)
+  end
+
+  private
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
+  end
+
+  def filter_foods(recipe, foods)
+    recipe_food_ids = RecipeFood.where(recipe_id: recipe.id).pluck(:food_id)
+
+    foods.reject { |food| recipe_food_ids.include?(food.id) }
+  end
+
+  def update_params
+    params.require(:recipe_food).permit(:quantity)
+  end
 end
