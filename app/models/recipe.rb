7e3ed9e6fc2b0c30d3da37ac33a74166a270173c
PabloBona <pablobonasera@gmail.com>
2023-10-11 11:57:48 -0300

    Fix Lint

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
-  
+
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
