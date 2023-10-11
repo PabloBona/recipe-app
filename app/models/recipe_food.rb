c1a360249e8ad58a52b5c010a8ab792842bb9bfe
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-11 02:08:40 -0600

    Add relationships and validations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,8 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  validates :quantity, presence: true
+  validates :recipe_id, presence: true
+  validates :food_id, presence: true
 end
