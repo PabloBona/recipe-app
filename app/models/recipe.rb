686226e5a9e3d86a77c0d4563ad27c20c8d03e54
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-11 23:42:59 -0600

    Set numericality validation, add required fields and refactor methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,8 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
 
   validates :name, presence: true
-  validates :preparation_time, presence: true
-  validates :cooking_time, presence: true
+  validates :preparation_time, numericality: { only_integer: true, greater_than: 0 }
+  validates :cooking_time, numericality: { only_integer: true, greater_than: 0 }
   validates :description, presence: true
   validates :user_id, presence: true
 end
